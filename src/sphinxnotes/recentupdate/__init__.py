ce3ce85d1435103b10a255edeab4b4f78b4518af
Shengyu Zhang <i@silverrainz.me>
2026-06-18 21:51:31 +0800

    refactor: Use a status_map

--- src/sphinxnotes/recentupdate/__init__.py
+++ src/sphinxnotes/recentupdate/__init__.py
@@ -48,16 +48,6 @@ class Revision:
     #: Git commit author date
     date: datetime
 
-    # FYI, possible status letters are:
-    # :A: addition of a file
-    # :C: copy of a file into a new one
-    # :D: deletion of a file
-    # :M: modification of the contents or mode of a file
-    # :R: renaming of a file
-    # :T: change in the type of the file
-    # :U: file is unmerged (you must complete the merge before it can be committed)
-    # :X: "unknown" change type (most probably a bug, please report it)
-
     #: List of docname, corresponding to files which are newly added
     added_docs: list[str]
     #: List of docname, corresponding to files which are modified
@@ -160,6 +150,17 @@ def get_git_revisions(
                     continue
                 a.append(docname)
         else:
+            # Possible status letters are:
+            # :A: addition of a file
+            # :C: copy of a file into a new one
+            # :D: deletion of a file
+            # :M: modification of the contents or mode of a file
+            # :R: renaming of a file
+            # :T: change in the type of the file
+            # :U: file is unmerged (you must complete the merge before it can be committed)
+            # :X: "unknown" change type (most probably a bug, please report it)
+            status_maps = {'M': m, 'A': a, 'D': d }
+
             # Use git diff --name-status with pathspecs for native pathspec matching
             name_status = repo.git.diff(
                 prev.hexsha, cur.hexsha, '--name-status', '--', *paths
@@ -172,17 +173,10 @@ def get_git_revisions(
                 if docname is None:
                     continue
 
-                if status == 'M':
-                    m.append(docname)
-                elif status == 'A':
-                    a.append(docname)
-                elif status == 'D':
-                    d.append(docname)
+                if status in status_maps:
+                    status_maps[status].append(docname)
                 else:
-                    logger.info(
-                        f'Skip {file_path}: '
-                        f'unsupported change type {status}'
-                    )
+                    logger.info(f'Skip {file_path}: unsupported change type {status}')
 
         if len(m) + len(a) + len(d) == 0:
             logger.debug(f'Skip commit {cur.hexsha}: no document changes')
